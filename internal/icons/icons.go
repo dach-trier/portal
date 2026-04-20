f633afdac7e665b1d88156d28bff331fc21d4a87
Volodymyr But <git@hiegz.com>
2026-04-20 18:46:58 +0200

    add icon template functions

---
+++ internal/icons/icons.go
@@ -0,0 +1,29 @@
+package icons
+
+import (
+	"bytes"
+	"html/template"
+)
+
+type Icons template.Template
+
+func New() *Icons {
+	var tmpl *template.Template
+
+	tmpl = template.New("")
+	tmpl = template.Must(tmpl.ParseGlob("icons/*.html"))
+
+	return (*Icons)(tmpl)
+}
+
+func (icons *Icons) FuncMap() template.FuncMap {
+	return template.FuncMap{
+		"icon": func(id string, attributes template.HTMLAttr) (template.HTML, error) {
+			var buf bytes.Buffer
+			if err := (*template.Template)(icons).ExecuteTemplate(&buf, id, attributes); err != nil {
+				return "", err
+			}
+			return template.HTML(buf.String()), nil
+		},
+	}
+}
