282546235d71b531f703e64e87e9cdc5f1180c4e
Volodymyr But <git@hiegz.com>
2026-04-21 01:55:25 +0200

    introduce initiative repositoty interface

---
+++ internal/repo/initiative.go
@@ -0,0 +1,22 @@
+package repo
+
+import (
+	"context"
+
+	"golang.org/x/text/language"
+
+	"github.com/dach-trier/portal/internal/model"
+)
+
+type InitiativeFilter struct {
+	Kind string
+}
+
+type InitiativeRepository interface {
+	ListTranslatedInitiativesWithThumbnail(
+		ctx context.Context,
+		lang language.Tag,
+		filter InitiativeFilter,
+		pagination Pagination,
+	) ([]model.TranslatedInitiativeWithThumbnail, error)
+}
