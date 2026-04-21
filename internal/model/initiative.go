2df62d89a53b85b3120f9f79692c17fbfbb409d2
Volodymyr But <git@hiegz.com>
2026-04-21 02:49:02 +0200

    add missing initiative model

---
+++ internal/model/initiative.go
@@ -0,0 +1,10 @@
+package model
+
+type TranslatedInitiativeWithThumbnail struct {
+	ID          string
+	Kind        string
+	ImageID     int32
+	ImageUrl    string
+	Name        string
+	Description string
+}
