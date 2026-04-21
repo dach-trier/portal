400eeceff206d20199b3fb5bab8d1f865da8214c
Volodymyr But <git@hiegz.com>
2026-04-21 02:35:49 +0200

    implement initiative repository for sqlc

---
+++ internal/repo/sqlc/initiative.go
@@ -0,0 +1,58 @@
+package sqlc_repo
+
+import (
+	"context"
+
+	"golang.org/x/text/language"
+
+	"github.com/dach-trier/portal/database/sqlc"
+	"github.com/dach-trier/portal/internal/model"
+	"github.com/dach-trier/portal/internal/repo"
+)
+
+type InitiativeRepository sqlc.Queries
+
+func NewInitiativeRepository(db sqlc.DBTX) *InitiativeRepository {
+	return (*InitiativeRepository)(sqlc.New(db))
+}
+
+func (repo *InitiativeRepository) ListTranslatedInitiativesWithThumbnail(
+	ctx context.Context,
+	lang language.Tag,
+	filter repo.InitiativeFilter,
+	pagination repo.Pagination,
+) ([]model.TranslatedInitiativeWithThumbnail, error) {
+	queries := (*sqlc.Queries)(repo)
+
+	result, err := queries.ListTranslatedInitiativesWithThumbnail(
+		ctx,
+		sqlc.ListTranslatedInitiativesWithThumbnailParams{
+			Lang:   lang.String(),
+			Kind:   filter.Kind,
+			Limit:  pagination.Limit,
+			Offset: pagination.Offset,
+		},
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	initiatives := []model.TranslatedInitiativeWithThumbnail{}
+
+	for _, row := range result {
+		initiatives = append(
+			initiatives,
+			model.TranslatedInitiativeWithThumbnail{
+				ID:          row.ID,
+				Kind:        row.Kind,
+				Name:        row.Name.String,
+				Description: row.Description.String,
+				ImageID:     row.ImageID,
+				ImageUrl:    row.ImageUrl,
+			},
+		)
+	}
+
+	return initiatives, nil
+}
