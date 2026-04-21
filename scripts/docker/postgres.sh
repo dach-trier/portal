38df7b20cbc7095707395b22054e58e4348b313b
Volodymyr But <git@hiegz.com>
2026-04-21 14:22:19 +0200

    add bash script to run postgres in docker with .env configuration

---
+++ scripts/docker/postgres.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -f .env ]; then
+    export $(grep -v '^#' .env | xargs)
+else
+    echo ".env file not found"
+    echo "note: .env file is required to configure the development database"
+    exit 1
+fi
+
+docker run -d \
+    -e POSTGRES_USER=$DB_USER \
+    -e POSTGRES_PASSWORD=$DB_PASS \
+    -e POSTGRES_DB=$DB_NAME \
+    -p $DB_PORT:5432 \
+    -v ./volumes/postgres:/var/lib/postgresql/18/data \
+    postgres:18.3-alpine3.22
